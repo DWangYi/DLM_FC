c123ed66f14ac7472af1278f62f1db080f1664d5
DWangYi <1138796322@qq.com>
2024-01-31 19:58:04 +0800

    2024.1.31

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
-import pyforest
-
+from pyforest import *
 # ================================================================
-### 变量说明：D——路段交通需求；N——路段总车道数；Nd——CAV专用道数量；p——路段CAV渗透率；K——车队规模; Dd——专用车道上的流量;\
-### Dg——通用车道上的流量; pd——专用车道上的渗透率; pg——通用车道上的渗透率; vd——专用车道上的速度; vg——通用车道上的速度;\
-### vd1——专用车道上的虚拟速度; vg1——通用车道上的虚拟速度
-D = 1000  # 路段交通需求
-N = 3  # 路段总车道数
-Nd = 1  # CAV专用道数量
-p = 0.1  # 路段CAV渗透率
-K = 5  # 最大车队规模
-Dd1 = p * D
-Dg1 = (1 - p) * D
-vd1 = vel(Dd1, cap(1, K))
-vg1 = vel(Dg1, cap(0, K))
-
+## 变量说明：D——路段交通需求；N——路段总车道数；Nd——CAV专用道数量；p——路段CAV渗透率；K——车队规模; Dd——专用车道上的流量;\
+## Dg——通用车道上的流量; pd——专用车道上的渗透率; pg——通用车道上的渗透率; vd——专用车道上的速度; vg——通用车道上的速度;\
+## vd1——专用车道上的虚拟速度; vg1——通用车道上的虚拟速度
 
 # ================================================================
 ## 单条车道通行能力计算函数，输入渗透率和最大车队规模，输出对应的通行能力
-def cap(p, k):  # p为渗透率，k为最大排规模
-
-    return 1800
+def cap(i, p, K):  # i为车道类型， p为渗透率，k为最大排规模，l为车身长度，S0为最小停车间距，vf为自由流速度，T为车辆安全车头时距
+    if i == 1: #专用车道
+        T = Hcc/K+(K-1)*Hcsc/K
+    else: #混合车道
+        A = 1-pow(p,K)
+        B = 1-p
+        C = 1-pow(p,K-1)
+        D = p*(1-p)        
+        T = B**2*Hhh + D*Hch + (p**2*C)*Hcsc/A + pow(p,K+1)*B*Hcc/A + D*Hhc
+    E = (l+S0)/vf
+    c = 1/(E+T)
+    return c*3600
 
 # ================================================================
 ## 基于BPR的速度计算函数，输入交通需求和通行能力，输出对应的车道速度
-def vel(Q, C):
-    t_0 = 1  # 自由流速度行驶
-    a = 1  # BPR函数中的alpha
-    b = 1  # BPR函数中的beta
+def vel(Q, C, vf):
+    t_0 = 1/vf  # 自由流速度行驶
+    a = 1.0122  # BPR函数中的alpha
+    b = 4.1856  # BPR函数中的beta
     t = t_0 * (1 + a * ((Q / C) ** b))
     v = 1 / t
     return v
@@ -48,23 +46,23 @@ def fuel_emission(i, v):  # i为指标类, 0——fuel, 1——HC, 2——NOx, 3
     return res
 
 # ================================================================
-## 速度均衡函数，输出车辆转移比例
+# 速度均衡函数，输出车辆转移比例
 def get_trans_2():  # 针对策略2的二分法;
     pd = 1
     pg = 0
     rate_high = p
     rate_low = 0
     rate = 0
-    diff = (p - rate) * D / cap(pd, K) - (1 - p + rate) * D / cap(pg, K)
-    while abs(diff) > 0.001:
+    diff = vel((p - rate) * D,Nd*cap(1, pd, K),vf) - vel((1 - p + rate) * D,(N-Nd)*cap(2, pg, K),vf)
+    while abs(diff) > 0.1:
         rate = (rate_high - rate_low) * 0.5 + rate_low
         pd1 = 1
         pg1 = rate / (1 - p + rate)
-        diff = (p - rate) / cap(pd1, K) - (1 - p + rate) / cap(pg1, K)
+        diff = vel((p - rate) * D,Nd*cap(1, pd1, K),vf) - vel((1 - p + rate) * D,(N-Nd)*cap(2, pg1, K),vf)
         if diff >= 0:
-            rate_low = rate
-        else:
             rate_high = rate
+        else:
+            rate_low = rate
     return rate
 
 def get_trans_3():  # 针对策略3的二分法;
@@ -73,16 +71,16 @@ def get_trans_3():  # 针对策略3的二分法;
     rate_high = 1 - p
     rate_low = 0
     rate = 0
-    diff = (p + rate) / cap(pd, K) - (1 - p - rate) / cap(pg, K)
-    while abs(diff) > 0.001:
+    diff = vel((p + rate)*D,Nd*cap(1, pd, K),vf) - vel((1 - p - rate)*D,(N-Nd)*cap(2, pg, K),vf)
+    while abs(diff) > 0.1:
         rate = (rate_high - rate_low) * 0.5 + rate_low
         pd1 = p / (p + rate)
         pg1 = 0
-        diff = (p + rate) / cap(pd1, K) - (1 - p - rate) / cap(pg1, K)
+        diff = vel((p + rate)*D,Nd*cap(2, pd1, K),vf) - vel((1 - p - rate)*D,(N-Nd)*cap(2, pg1, K),vf)
         if diff >= 0:
-            rate_high = rate
-        else:
             rate_low = rate
+        else:
+            rate_high = rate
     return rate
 
 # ================================================================
@@ -127,21 +125,36 @@ def flow_dis(i):
 
 # ================================================================
 ##已知参数为路段交通需求、路段总车道数、CAVs专用道数量、路段CAV渗透率、最大车队规模
-D = 1000  # 路段交通需求
+D = 5000 # 路段交通需求
 N = 3  # 路段总车道数
 Nd = 1  # CAV专用道数量
-p = 0.1  # 路段CAV渗透率
+p = 0.8  # 路段CAV渗透率
 K = 5  # 最大车队规模
+Hhh = Hch = 1.52
+Hhc = 1.35
+Hcsc = 0.65
+Hcc = 0.85 #车头时距
+l = 5 #车身长度
+S0 = 2 #最小停车间距
+vf = 33.3 #自由流速度
 
-dis = flow_dis(2)
-Dd = dis[0]
-Dg = dis[1]
-pd = dis[2]
-pg = dis[3]
-cap_d = cap(pd, K)   #专用车道通行能力
-cap_g = (N-Nd)*cap(pg, K)   #通用车道通行能力
-v_d = vel(Dd, cap_d)
-v_g = vel(Dg, cap_g)
-fuel_d = fuel_emission(0, v_d)
-fuel_g = fuel_emission(0, v_g)
-fuel_road = (fuel_d*Dd + fuel_g*Dg)/D
+Dd1 = p * D
+Dg1 = (1 - p) * D
+vd1 = vel(Dd1, Nd*cap(1, 1, K), vf) #虚拟速度
+vg1 = vel(Dg1, (N-Nd)*cap(2, 0, K), vf)
+dis = flow_dis(2) #策略i的流量分布
+Dd = dis[0] #专用车道的需求
+Dg = dis[1] #混合车道的需求
+pd = dis[2] #专用车道的渗透率
+pg = dis[3] #混合车道的渗透率
+cap_d = Nd*cap(1, pd, K)   #专用车道通行能力
+cap_g = (N-Nd)*cap(2, pg, K)   #通用车道通行能力
+v_d = vel(Dd, cap_d, vf) #均衡速度
+v_g = vel(Dg, cap_g, vf) #均衡速度
+
+# fuel_d = fuel_emission(0, v_d)
+# fuel_g = fuel_emission(0, v_g)
+# fuel_ave = (fuel_d * Dd + fuel_g * Dg) / (Dd + Dg)
+
+print('虚拟速度:'"{:.3f}".format(vd1),"{:.3f}".format(vg1))
+print('均衡速度:'"{:.3f}".format(v_d),"{:.3f}".format(v_g))
